07c3945f8e4e220c569abf5afea4dea51ed18d97
Danielle Kom <daniellekom@Danielles-MacBook-Air.local>
2022-01-20 14:52:35 -0500

    day-four

---
+++ index.js
@@ -0,0 +1,22 @@
+// function readsTheData()
+// {
+// const fs = require('fs')
+// const data = fs.readFileSync('data.json',"utf-8")
+// return data
+// }
+// console.log(readsTheData())
+
+
+
+
+function readsTheData(){
+
+
+const fs = require('fs')
+fs.readFile('data.json', (err, data) => {
+if (err) throw err
+console.log(data.toString('utf-8'))
+})
+console.log('hi')
+}  
+readsTheData()
